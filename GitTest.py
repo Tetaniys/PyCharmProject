7c52fd3f0d320c8e99df82dd88121b77e36ac7e7
Oleg <goncharenko.olegator@gmail.com>
2025-10-26 23:16:19 +0300

    YRA

--- GitTest.py
+++ GitTest.py
@@ -1,2 +1,17 @@
-print('This is bed')
-print('no, success')
+class A:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def mul(self):
+        return self.x * self.y
+
+    @staticmethod
+
+    def mul_any(x, y):
+        return x**y
+
+
+a = A(10, 15)
+print(a.mul())
+print(a.mul_any(10, 15))
